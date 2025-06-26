0f340dabe511fe2cd6ad3fd236587815e98b3849
rh0 <125304115+rehite1@users.noreply.github.com>
2025-06-27 02:30:54 +0530

    malloc overhead checked

---
+++ malloc_overhead/test.c
@@ -0,0 +1,54 @@
+#include <alloca.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <alloca.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/resource.h>
+#include "test_data.h"
+
+void test(ulong size);
+void control(ulong size);
+void alloca_limit();
+int main(){
+	//test(TEST_SMALL);
+	//test(TEST_MID);
+	//test(TEST_LARGE);
+	test(TEST_VLARGE);
+	//while(1) alloca_limit();
+	//control(TEST_MID);
+	//control(TEST_LARGE);
+}
+void test(ulong size){
+	void* arr[num_alloc];	
+	for(int i=0;i<num_alloc;++i){
+		arr[i]=malloc(size);
+	}
+	for(int i=0;i<num_alloc;++i){
+		free(arr[i]);
+	}
+	printf("num allocated:%d\nsize:%lu\n",num_alloc,size);
+}
+void control(ulong size){
+	struct rlimit lim;
+	assert(0==getrlimit(RLIMIT_STACK,&lim));
+	assert(size*num_alloc-5000<lim.rlim_cur&&"too big for stack allocation");
+	// printf("%lu:%lu\n",lim.rlim_max,lim.rlim_cur);
+	void* arr[num_alloc];	
+	for(int i=0;i<num_alloc;++i){
+		arr[i]=alloca(size);
+	}
+	
+	for(int i=0;i<num_alloc;++i){
+		(void)arr[i];
+	}
+	printf("num allocated:%d\nsize:%lu\n",num_alloc,size);
+}
+void alloca_limit(){
+	static ulong i=0;
+	void* a=alloca(i+=128);
+	(void)a;
+	printf("%lu\n",i);
+}
+
+
---
+++ malloc_overhead/test_data.h
@@ -0,0 +1,5 @@
+#define num_alloc	1000000
+#define TEST_SMALL 	8
+#define TEST_MID	64
+#define TEST_LARGE	256*8
+#define TEST_VLARGE	1024*8
